03f3b950d7194a27b3de22edb481ccff44ee97b0
jjeeccss <jeicogacusana24@gmail.com>
2024-11-04 23:07:46 +0800

    initial commit

---
+++ classSubjects.js
@@ -0,0 +1,6 @@
+let Subject = "PL";
+let classSchedule = "Monday and Thuesday ,8:00 - 9:00 am";
+let classroom = "Comlab B";
+let classInstructor = " Mr. Kim Dave Rafael";
+let studentName = "John Jeico Gacusana";
+console.log(`${studentName} is currently enrolled in ${Subject} with a class schedule of ${classSchedule} at ${classroom}. The instructor for this subject is ${classInstructor}.`);
